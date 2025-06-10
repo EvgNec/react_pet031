604b84f717efbb5f941ae338e91d8d399c67d2a7
Evgeniy <evgnec@gmail.com>
2025-06-10 20:24:01 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import Container from './Container';
 import { Player } from './Player/Player';
 import VideoList from './VideoList/VideoList';
 import videos from './videos.json';
+import Reader from './Reader/Reader';
+import news from './publication.json';
 
 export class App extends Component {
   state = {
@@ -17,11 +19,16 @@ export class App extends Component {
     const { selectVideo } = this.state;
     return (
       <>
-        <Container>
+<Container>
+  <Reader news={news}/>
+</Container>
+
+
+        {/* <Container>
           <h1> Select video: {selectVideo}</h1>
           <VideoList videos={videos} onSelect={this.selectVideo} />
           <Player url={selectVideo}/>
-        </Container>
+        </Container> */}
       </>
     );
   }
---
+++ src/components/Reader/Reader.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Reader extends Component {
+    state = {
+        publicationIndex: 0
+    }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Reader
