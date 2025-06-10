1431abc18e7b114d66420668d443256619fb829b
Evgeniy <evgnec@gmail.com>
2025-06-10 20:29:52 +0300

    upd

--- src/components/Reader/Reader.jsx
+++ src/components/Reader/Reader.jsx
@@ -5,9 +5,20 @@ export class Reader extends Component {
         publicationIndex: 0
     }
   render() {
+    const {news} =this.props;
     return (
       <div>
-        
+        <section>
+            <button type='button'>Next</button>
+            <button type='button'>Preview</button>
+        </section>
+        <p>1/10</p>
+        {news.map(({ id, title, text }) => (
+        <article key={id}>
+            <h2>{title}</h2>
+            <p>{text}</p>
+        </article>
+        ))}
       </div>
     )
   }
