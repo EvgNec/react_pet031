8a93d73ab589918b4574beb717c3a22d30c9f2cd
Evgeniy <evgnec@gmail.com>
2025-06-10 20:51:50 +0300

    upd

--- src/components/Reader/Reader.jsx
+++ src/components/Reader/Reader.jsx
@@ -1,41 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class Reader extends Component {
-    state = {
-        index: 0
-    }
+  state = {
+    index: 0,
+  };
 
-changeIndex = (value) => {
-    this.setState(state => ({index: state.index + value}))
-}
+  changeIndex = value => {
+    this.setState(state => ({ index: state.index + value }));
+  };
 
-// onPrev = () => {
-//     this.setState(state => ({index: state.index - 1}))
-// }
-// onNext = () => {
-//     this.setState(state => ({index: state.index + 1}))
-// }
-
+  // onPrev = () => {
+  //     this.setState(state => ({index: state.index - 1}))
+  // }
+  // onNext = () => {
+  //     this.setState(state => ({index: state.index + 1}))
+  // }
 
   render() {
-    const {news} =this.props;
+    const { news } = this.props;
+    const totalNews = news.length;
+    const currentItem = this.props.news[this.state.index];
     const { index } = this.state;
     return (
       <div>
         <section>
-            <button type='button' onClick={() => this.changeIndex(-1)}>Next</button>
-            <button type='button' onClick={() => this.changeIndex(1)}>Preview</button>
+          <button 
+          type="button" 
+          disabled={index === 0}
+          onClick={() => this.changeIndex(-1)}>
+            Next
+          </button>
+          <button 
+          type="button" 
+          disabled={index === totalNews-1}
+          onClick={() => this.changeIndex(1)}>
+            Preview
+          </button>
         </section>
-        <p>{index+1}/{news.length}</p>
-        {news.map(({ id, title, text }) => (
-        <article key={id}>
-            <h2>{title}</h2>
-            <p>{text}</p>
+        <p>
+          {index + 1}/{totalNews}
+        </p>
+
+        <article key={currentItem.id}>
+          <h2>{currentItem.title}</h2>
+          <p>{currentItem.text}</p>
         </article>
-        ))}
       </div>
-    )
+    );
   }
 }
 
-export default Reader
+export default Reader;
